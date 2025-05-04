f17a2f949e2683624ed58a37743864465d2e7249
davidwangcn9 <yichen.wang@thoughtworks.com>
2025-05-05 01:41:12 +0800

    test

--- shadow-maker/application.js
+++ shadow-maker/application.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.json';
+          this.settingsPath = 'shadow-maker/src/settings.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
